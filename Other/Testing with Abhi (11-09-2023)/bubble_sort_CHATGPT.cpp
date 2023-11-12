2decf47cbd5be72c3d2528bf72b5578fd8fb5311
Jonathan Agarrado (Desktop) <jet320boy@gmail.com>
2023-11-13 00:27:52 +0800

    Refactor bubble sort to use std::vector

--- Other/Testing with Abhi (11-09-2023)/bubble_sort_CHATGPT.cpp
+++ Other/Testing with Abhi (11-09-2023)/bubble_sort_CHATGPT.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
+#include <vector>
 
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+void bubbleSort(std::vector<int>& arr) {
+  for (int i = 0; i < arr.size() - 1; i++) {
+    for (int j = 0; j < arr.size() - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
+  }
 }
 
 int main() {
-    int n;
+  int n;
 
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n;
+  std::cout << "Enter the number of elements: ";
+  std::cin >> n;
 
-    if (n <= 0) {
-        std::cout << "Invalid input. Please enter a positive number of elements." << std::endl;
-        return 1;
-    }
+  if (n <= 0) {
+    std::cout << "Invalid input. Please enter a positive number of elements." << std::endl;
+    return 1;
+  }
 
-    int arr[n];
+  std::vector<int> arr(n); // Create a vector of size n
 
-    std::cout << "Enter " << n << " integers:" << std::endl;
+  std::cout << "Enter " << n << " integers:" << std::endl;
 
-    for (int i = 0; i < n; i++) {
-        std::cin >> arr[i];
-    }
+  for (int i = 0; i < n; i++) {
+    std::cin >> arr[i];
+  }
 
-    bubbleSort(arr, n);
+  bubbleSort(arr);
 
-    std::cout << "Sorted array in ascending order: ";
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
+  std::cout << "Sorted array in ascending order: ";
+  for (int i = 0; i < arr.size(); i++) {
+    std::cout << arr[i] << " ";
+  }
+  std::cout << std::endl;
 
-    return 0;
+  return 0;
 }
