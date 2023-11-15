3544578c84c82abad6c32ae12587779f68b8148e
DalanDan <jet320boy@gmail.com>
2023-11-15 09:17:58 +0800

    Initial commit

---
+++ Other/Self-Test/while-test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    cout << endl;
+
+
+    int i = 0;
+    while (i < 5)
+    {
+        cout << i << endl;
+        i++;
+    }
+
+    cout << endl;
+
+    return 0;
+}
